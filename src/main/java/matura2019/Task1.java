a451356c22a88b6ad8440ff923bd640411f6f70d
toomec2105 <tomec2105@interia.pl>
2020-06-27 19:29:59 +0200

    binary search

--- src/main/java/matura2019/Task1.java
+++ src/main/java/matura2019/Task1.java
@@ -5,9 +5,9 @@ public class Task1 {
 	public Integer findFirstEvenWithForLoop_Linear(int[] A) {
 		int w = 0;
 		int n = A.length;
-	
+
 		for (int i = 0; i < n; i++) {
-			
+
 			if (A[i] % 2 == 0) {
 				w = A[i];
 				return w;
@@ -20,21 +20,42 @@ public class Task1 {
 	public Integer findFirstEvenWithWhileLoop_Linear(int[] A) {
 		int i = 0;
 		int w = 0;
-	
+
 		while (A[i] % 2 == 1) {
 			i++;
 		}
-		
+
 		w = A[i];
-		
+
 		return w;
 	}
 
-	public Integer findFirstEven_Logarithmic(int[] twoNumberArray) {
-		// TODO Auto-generated method stub
+	public Integer findFirstEven_Logarithmic(int[] A) {
+		int w = 0;
+		int n = A.length;
+
 		return null;
 	}
 
-	
+	public Integer binarySearch(int[] A) {
+
+		int start = 0;
+		int end = A.length;
+
+		while (start <= end) {
+			int middle = (start + end) / 2;
+
+			if (A[middle] == 15) {
+				return 15;
+			}
+			if (A[middle] > 15) {
+				end = middle - 1;
+			}
+			if (A[middle] < 15) {
+				start = middle + 1;
+			}
+		}
+		return null;
+	}
 
 }
--- src/test/java/user/Task1_LogarithmicTest.java
+++ src/test/java/user/Task1_LogarithmicTest.java
@@ -1,21 +1,43 @@
 package user;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import matura2019.Task1;
 
-
 public class Task1_LogarithmicTest {
-	private int[] twoNumberArray = {1,2};
-	private int[] fourNumberArray = {1,5,6,2};
-
+	private int[] twoNumberArray = { 1, 2 };
+	private int[] fourNumberArray = { 1, 5, 6, 2 };
+	private int[] sevenNumberArrayWith15 = { 1, 8, 15, 17, 99, 100, 102 };
 	private Task1 task1 = new Task1();
+
+	@Disabled
 	@Test
 	void givenTwoNumbers_findsFirstEven() {
 		assertEquals(2, task1.findFirstEven_Logarithmic(twoNumberArray));
+
+	}
+
+	@Test
+	void binarySearchTest() {
 		
+		int[][] nums = {
+				  {15},
+				  {15,20},
+				  {20,15},
+				  {3,15,20},
+				  {15,16,20},
+				  {1,2,15},
+				  {1,2,15,20},
+				  {5,15,17,20},
+				  {1,10,11,14,15,20},
+				  {15,16,19,200,210,215}
+			};
+		for (int[] arr : nums) {
+			
+			assertEquals(15, task1.binarySearch(arr));
+		}
 	}
 }
