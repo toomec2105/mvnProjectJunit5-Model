09932a118fc28cd66ee22843c1845be2ecd9c004
toomec2105 <tomec2105@interia.pl>
2020-07-10 19:24:51 +0200

    task 5.3

--- src/main/java/matura2019/part2/Task5.java
+++ src/main/java/matura2019/part2/Task5.java
@@ -5,7 +5,11 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Task5 {
 	public static void main(String[] args) {
@@ -19,38 +23,98 @@ public class Task5 {
 		 * countHotAndDryDays(weatherData));
 		 */
 		// ----------------------5.2-----------------------
-		List<WeatherData> dayTemp = getDayTemp(strings);
-		List<Double> temperatures = getTemperatures(weatherData);
-		System.out.println("Expected: 19 Actual " + temperatures.get(0));
 
+		/*
+		 * List<WeatherData> dayTemp = getDayTemp(strings); List<Double> temperatures =
+		 * getTemperatures(weatherData); System.out.println("Expected: 19 Actual " +
+		 * temperatures.get(0));
+		 * 
+		 * 
+		 * System.out.println("Expected: [36,38] Actual: " + getLongestSequence(
+		 * Arrays.asList(new WeatherData(34, 3.7), new WeatherData(35, 3.4), new
+		 * WeatherData(36, 3.5), new WeatherData(37, 3.6), new WeatherData(38, 3.7), new
+		 * WeatherData(39, 3.5))));
+		 * 
+		 * 
+		 * System.out.println("Expected: ??? Actual: " + getLongestSequence(dayTemp));
+		 */
+		List<WeatherData> dayRainCloud = getDayRainCloud(strings).subList(0, 300);
+		System.out.println(
+				"Expected: C1:7.5, C2:1, S1: 4 Actual: " + getAverageRain(Arrays.asList(new WeatherData(1, 10, "C", 1),
+						new WeatherData(1, 5, "C", 1), new WeatherData(1, 1, "C", 2), new WeatherData(1, 4, "S", 1))));
+		Map<String, Double> result = getAverageRain(dayRainCloud);
+		List<String> clouds = new ArrayList<>(result.keySet());
+		Collections.sort(clouds);
+		for (String cloud : clouds) {
+			System.out.printf("%s : %.2f \n", cloud, result.get(cloud));
+		}
+
+	}
+
+	private static List<WeatherData> getDayRainCloud(List<String> strings) {
+		List<WeatherData> weatherData = new ArrayList<>();
+
+		for (String data : strings) {
+			String[] row = data.split(";");
+			weatherData.add(new WeatherData(Integer.parseInt(row[0]), Integer.parseInt(row[2]), row[3],
+					Integer.parseInt(row[4])));
+		}
+		return weatherData;
+	}
+
+	private static Map<String, Double> getAverageRain(List<WeatherData> weatherData) {
+		List<WeatherData> rainData = new ArrayList<>(weatherData);
+		Map<String, List<Double>> averages = new HashMap<>();
+		Map<String, Double> output = new HashMap<>();
 		
-		  System.out.println("Expected: [36,38] Actual: " + getLongestSequence(
-		  Arrays.asList(new WeatherData(34, 3.7), new WeatherData(35, 3.4), new
-		  WeatherData(36, 3.5), new WeatherData(37, 3.6), new WeatherData(38, 3.7), new
-		  WeatherData(39, 3.5))));
-		 
+		for (int i = 1; i <= 5; i++) {
+			averages.put("C" + i, Arrays.asList(0.0, 0.0));
+			averages.put("S" + i, Arrays.asList(0.0, 0.0));
+		}
 		
-		System.out.println("Expected: ??? Actual: " + getLongestSequence(dayTemp));
+		for (WeatherData dailyData : rainData) {
+			int cloudSize = dailyData.getSize();
+			int rainFall = dailyData.getRain();
+			String cloudNameType = dailyData.getCategory() + cloudSize; 
+			
+			if (dailyData.getCategory().equals("0")) {
+				continue;
+			} 
+				Double totalRainFall = averages.get(cloudNameType).get(0) + rainFall;
+				Double daysWithSameCloudType = averages.get(cloudNameType).get(1) + 1;
+				
+				averages.put(cloudNameType, Arrays.asList(totalRainFall, daysWithSameCloudType));
+			
+		}
+		
+		for (int i = 1; i <= 5; i++) {
+			Double cTypeCloudAverage = averages.get("C" + i).get(0) / averages.get("C" + i).get(1);
+			Double sTypeCloudAverage =averages.get("S" + i).get(0) / averages.get("S" + i).get(1);
+			output.put("C" + i, cTypeCloudAverage);
+			output.put("S" + i, sTypeCloudAverage);
+		}
+		return output;
 	}
 
 	private static List<Integer> getLongestSequence(List<WeatherData> dayTemp) {
 		List<Integer> firstLast = new ArrayList<>();
 		firstLast.add(1);
 		firstLast.add(2);
-		
+
 		for (int i = 0; i < dayTemp.size() - 1; i++) {
-			
+
 			for (int j = 0; j < dayTemp.size() - 1; j++) {
-				
+
 				Double temp1 = dayTemp.get(i + j).getTemp();
 				Double temp2 = dayTemp.get(i + j + 1).getTemp();
-				
+
 				if (temp1 >= temp2) {
-					
+
 					break;
 				} else {
-					
-					if (firstLast.get(1) - firstLast.get(0) < dayTemp.get(i + j + 1).getDayID() - dayTemp.get(i).getDayID()) {
+
+					if (firstLast.get(1) - firstLast.get(0) < dayTemp.get(i + j + 1).getDayID()
+							- dayTemp.get(i).getDayID()) {
 						firstLast.set(0, dayTemp.get(i).getDayID());
 						firstLast.set(1, dayTemp.get(j + i + 1).getDayID());
 					}
@@ -127,6 +191,14 @@ class WeatherData {
 		super();
 		this.dayID = dayID;
 		this.temp = temp;
+		this.rain = rain;
+		this.category = category;
+		this.size = size;
+	}
+
+	public WeatherData(Integer dayID, Integer rain, String category, Integer size) {
+		super();
+		this.dayID = dayID;
 		this.rain = rain;
 		this.category = category;
 		this.size = size;
